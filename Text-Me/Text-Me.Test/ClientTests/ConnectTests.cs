18e15c7ac61ac6f6a4255445c24520903aa3c76a
ali elmas <a.ihsan.elmas@gmail.com>
2021-10-17 11:21:33 +0300

    Create a helper class for unit tests

--- Text-Me/Text-Me.Test/ClientTests/ConnectTests.cs
+++ Text-Me/Text-Me.Test/ClientTests/ConnectTests.cs
@@ -15,18 +15,6 @@ namespace Text_Me.Test.ClientTests
     public class ConnectTests
     {
 
-        private static readonly IPEndPoint DefaultLoopbackEndpoint = new IPEndPoint(IPAddress.Loopback, port: 0);
-
-        public static int GetAvailablePort()
-        {
-            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-            {
-                socket.Bind(DefaultLoopbackEndpoint);
-                return ((IPEndPoint)socket.LocalEndPoint).Port;
-            }
-        }
-
-
         [Fact]
         public void ClientShouldConnectToAServer()
         {
@@ -64,8 +52,8 @@ namespace Text_Me.Test.ClientTests
             Client client = new Client();
             client.OnConnection += ResultFunc;
             ConnectionResult connectionResult = ConnectionResult.UNKNOWN;
-            
-            client.Connect(IPAddress.Loopback.ToString(), GetAvailablePort());
+
+            client.Connect(IPAddress.Loopback.ToString(), Helpers.GetAvailablePort());
             void ResultFunc(ConnectionResult result)
             {
                 connectionResult = result;
