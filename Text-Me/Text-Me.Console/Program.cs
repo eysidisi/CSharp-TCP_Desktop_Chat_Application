804f1613a91fc082c412bfea0ff1f384b20d97ef
ali elmas <a.ihsan.elmas@gmail.com>
2021-10-22 21:32:47 +0300

    Add check to see if it's ready to read

--- Text-Me/Text-Me.Console/Program.cs
+++ Text-Me/Text-Me.Console/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Net;
+using Text_Me.Service;
+using static Text_Me.Service.Client;
+
+Client Client = new Client();
+
+Client.Connect(IPAddress.Loopback.ToString(), 63909);
+
+Console.ReadLine();
+
+
+void ConnectionResultCallback(ConnectionResult result)
+{
+    Console.WriteLine(result);
+}
--- Text-Me/Text-Me.Service/Server.cs
+++ Text-Me/Text-Me.Service/Server.cs
@@ -63,11 +63,10 @@ namespace Text_Me.Service
             int numberOfBytesReceived;
 
             // Loop to receive all the data sent by the client.
-            while ((numberOfBytesReceived = stream.Read(receivedBytes, 0, receivedBytes.Length)) != 0)
+            while (stream.CanRead && (numberOfBytesReceived = stream.Read(receivedBytes, 0, receivedBytes.Length)) != 0)
             {
                 // Translate data bytes to a ASCII string.
                 string receivedMessage = Encoding.UTF8.GetString(receivedBytes, 0, numberOfBytesReceived);
-                Console.WriteLine("Received: {0}", receivedMessage);
 
                 // Send back a response.
                 OnMessageReceived?.Invoke(receivedMessage);
