fe9a8fe6582860f2d3f2956e0238c76e316c04d1
ali elmas <a.ihsan.elmas@gmail.com>
2021-10-29 12:30:00 +0300

    Text server can send and receive messages #63

---
+++ Text-Me/Text-Me-Server.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using Text_Me.Service;
+
+int serverPortNum = 49303;
+
+Server server = new Server(IPAddress.Loopback.ToString(), serverPortNum);
+server.OnConnection += ClientConnection;
+server.OnMessageReceived += ClientMessageReceived;
+
+server.StartAcceptingConnection();
+
+while (true)
+{
+    string? input = Console.ReadLine();
+
+    if (input == null)
+    {
+        continue;
+    }
+    if (input == "qq")
+    {
+        break;
+    }
+
+    server.SendMessage(input);
+}
+
+
+void ClientConnection(ConnectionResult result)
+{
+    Console.WriteLine($"Connection resulted in {result}");
+}
+void ClientMessageReceived(string receivedMessage)
+{
+    Console.WriteLine($"Received: {receivedMessage}");
+}
